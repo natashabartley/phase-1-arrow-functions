36b6b2e0dadaef303b4ebe0f1b99cc2f69f1c5fc
Natasha Bartley <+>
2023-03-25 16:17:35 -0400

    completed passing lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a,b) => {
+    a = 2000;
+    b = 100;
+        return a / b 
+} 
+
+let x = 2;
+const square = x =>  x * x;
+
+const add = (a,b) => {
+    a = 3;
+    b = 4;
+    return a + b;
+}
